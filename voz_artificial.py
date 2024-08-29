240c3cbb3cb763444c513e919ef7382a19363d12
Marcelo Zepeda <marcelozepe.unam@gmail.com>
2024-08-29 15:58:48 -0600

    Se hizo un entrenamiento de la voz artificial, para utilizar la API con la voz de Veggeta

--- voz_artificial.py
+++ voz_artificial.py
@@ -1,13 +1,9 @@
-'''Este modulo genera una recomendacion a traves de la API de openai, esta recomendacion es creada con base a los datos de lectura_iuv (lectura del sensor) y categoria.
-Se utilizó tecnicas de prompt engineer como "few-shots" la cual le muestra algunos ejemplos de como se desea el resultado, de esta forma se evitan alucionaciones de GPT-4'''
-
-#Modulo voz_artificial
-
 import requests
 import os
 import pygame
 import logging
 from dotenv import load_dotenv
+from queue import Queue  # Asegúrate de importar Queue si lo estás utilizando
 
 # Configuración del logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -15,6 +11,11 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 def genera_voz_artificial(data_queue):
     load_dotenv()
     XI_API_KEY = os.getenv("ELEVEN_API_KEY")
+
+    if XI_API_KEY is None:
+        logging.error("API Key no configurada correctamente. Verifica tu archivo .env.")
+        return
+
     url = "https://api.elevenlabs.io/v1/text-to-speech/OPa7mOx679yrlEZ1ooEi"
     headers = {
         "Accept": "audio/mpeg",
@@ -27,6 +28,9 @@ def genera_voz_artificial(data_queue):
     while True:
         try:
             respuesta_de_recomendacion = data_queue.get()
+            if respuesta_de_recomendacion is None:  # Salir del bucle si no hay datos
+                break
+
             data = {
                 "text": respuesta_de_recomendacion,
                 "model_id": "eleven_multilingual_v2",
@@ -39,7 +43,7 @@ def genera_voz_artificial(data_queue):
             response = requests.post(url, json=data, headers=headers)
 
             if response.status_code == 200 and len(response.content) > 1024:
-                logging.warning(f"Respuesta no exitosa de la API de voz. Estado: {response.status_code}, Longitud del contenido: {len(response.content)}")
+                logging.info(f"Respuesta exitosa de la API de voz. Estado: {response.status_code}, Longitud del contenido: {len(response.content)}")
                 audio_file = 'output.mp3'
                 with open(audio_file, 'wb') as f:
                     f.write(response.content)
@@ -53,15 +57,12 @@ def genera_voz_artificial(data_queue):
                 pygame.mixer.music.stop()
                 os.remove(audio_file)
 
-                logging.info(f"Voz generada y reproducida con éxito para el texto: {respuesta_de_recomendacion}") #? Logging info
+                logging.info(f"Voz generada y reproducida con éxito para el texto: {respuesta_de_recomendacion}")
             else:
-                logging.warning("La respuesta de la API de voz no fue exitosa o el contenido es demasiado corto.") #? Logging info
+                logging.warning("La respuesta de la API de voz no fue exitosa o el contenido es demasiado corto.")
 
         except Exception as e:
-            logging.error(f"Error al generar voz artificial: {e}") #? Logging info
+            logging.error(f"Error al generar voz artificial: {e}")
             break  # O manejar de otra forma
 
     pygame.mixer.quit()
-
-
-
